15d8ee18fe803161e6bf65d01c9c143ddb427f9d
leafcabral <rafaelcabral4100@gmail.com>
2026-06-08 12:38:47 -0300

    chore: initial commit with files

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Tarefa 6</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<button></button>
+
+	<script>
+		Laele
+	</script>
+</body>
+</html>
