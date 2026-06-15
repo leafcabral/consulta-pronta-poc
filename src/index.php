b8719527cb2a717838286aaa4f60fe08668245e7
leafcabral <rafaelcabral4100@gmail.com>
2026-06-14 22:22:44 -0300

    feat: basic html and css for overall pages

--- src/index.php
+++ src/index.php
@@ -4,13 +4,23 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Tarefa 6</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-	<button></button>
+	<aside>
+		<div id="aside_up">
+			icon and navigation
+		</div>
 
-	<script>
-		Laele
-	</script>
+		<div id="aside_bottom">
+			profile and notification
+		</div>
+	</aside>
+
+
+	<main>
+		<h1>Placeholder</h1>
+		<h2>Made to place your holder</h2>
+	</main>
 </body>
 </html>
