8702daeab63671904b2b68f8bef0459336519e9a
calielian <calielian@proton.me>
2026-06-22 20:01:37 -0300

    fix: wrong cardinality in Relatorio ---> Anotacao_Clinica and no titulo atribute on Relatorio

--- db_generator.sql
+++ db_generator.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS usuario (
 
     CONSTRAINT FK_USUARIO_id_contato
         FOREIGN KEY (id_contato)
-        REFERENCES contato(id_contato);
+        REFERENCES contato(id_contato)
 );
 
 CREATE TABLE IF NOT EXISTS paciente (
@@ -44,7 +44,7 @@ CREATE TABLE IF NOT EXISTS profissional (
 
     CONSTRAINT FK_PROFISSIONAL_id_usuario
         FOREIGN KEY (id_profissional)
-        REFERENCES usuario(id_usuario);
+        REFERENCES usuario(id_usuario)
 );
 
 CREATE TABLE IF NOT EXISTS sintoma (
@@ -58,7 +58,7 @@ CREATE TABLE IF NOT EXISTS sintoma (
 
     CONSTRAINT FK_SINTOMA_id_paciente
         FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente);
+        REFERENCES paciente(id_paciente)
 );
 
 CREATE TABLE IF NOT EXISTS autorizacao (
@@ -74,7 +74,7 @@ CREATE TABLE IF NOT EXISTS autorizacao (
         REFERENCES profissional(id_profissional);
     CONSTRAINT FK_AUTORIZACAO_id_paciente
         FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente);
+        REFERENCES paciente(id_paciente)
 );
 
 CREATE TABLE IF NOT EXISTS prescricao (
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS prescricao (
         REFERENCES profissional(id_profissional);
     CONSTRAINT FK_PRESCRICAO_id_paciente
         FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente);
+        REFERENCES paciente(id_paciente)
 );
 
 CREATE TABLE IF NOT EXISTS exame (
@@ -108,12 +108,26 @@ CREATE TABLE IF NOT EXISTS exame (
         REFERENCES paciente(id_paciente);
     CONSTRAINT FK_EXAME_id_profissional
         FOREIGN KEY (id_profissional)
-        REFERENCES profissional(id_profissional);
+        REFERENCES profissional(id_profissional)
+);
+
+CREATE TABLE IF NOT EXISTS relatorio (
+    id_relatorio INTEGER PRIMARY KEY,
+    id_paciente INTEGER,
+    data_geracao DATE,
+    titulo VARCHAR(100),
+    periodo_analisado VARCHAR(30),
+    dados_analiticos VARCHAR(200),
+
+    CONSTRAINT FK_RELATORIO_id_paciente
+        FOREIGN KEY (id_paciente)
+        REFERENCES paciente(id_paciente)
 );
 
 CREATE TABLE IF NOT EXISTS anotacao_clinica (
     id_anotacao INTEGER PRIMARY KEY,
     id_profissional INTEGER,
+    id_relatorio INTEGER,
     data_hora TIMESTAMP,
     texto_evolucao VARCHAR(300),
     hipotese_diagnostica VARCHAR(100),
@@ -121,20 +135,7 @@ CREATE TABLE IF NOT EXISTS anotacao_clinica (
     CONSTRAINT FK_ANOTACAO_CLINICA_id_profissional
         FOREIGN KEY (id_profissional)
         REFERENCES profissional(id_profissional);
-);
-
-CREATE TABLE IF NOT EXISTS relatorio (
-    id_relatorio INTEGER PRIMARY KEY,
-    id_paciente INTEGER,
-    id_anotacao INTEGER,
-    data_geracao DATE,
-    periodo_analisado VARCHAR(30),
-    dados_analiticos VARCHAR(200),
-
-    CONSTRAINT FK_RELATORIO_id_paciente
-        FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente);
-    CONSTRAINT FK_RELATORIO_id_anotacao
-        FOREIGN KEY (id_anotacao)
-        REFERENCES anotacao_clinica(id_anotacao);
+    CONSTRAINT FK_ANOTACAO_CLINICA_id_relatorio
+        FOREIGN KEY (id_relatorio)
+        REFERENCES relatorio(id_relatorio)
 );
