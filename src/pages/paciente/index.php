b6079478a2c24eccbe8b31396e3267ad4f6bae36
leafcabral <rafaelcabral4100@gmail.com>
2026-06-22 17:55:23 -0300

    feat: improve patient dashboard style

--- src/pages/paciente/index.php
+++ src/pages/paciente/index.php
@@ -8,6 +8,7 @@
 	<link rel="shortcut icon" href="<?= SRC_URL ?>/icons/icon.png" type="image/x-icon">
 	<title>Dashboard</title>
 	<link rel="stylesheet" href="<?= SRC_URL ?>/styles/style.css">
+	<link rel="stylesheet" href="paciente.css">
 </head>
 <body>
 	<?php require_once "../../includes/aside.php" ?>
@@ -18,72 +19,27 @@
 			<h2>Seja bem vindo, <?= "PLACEHOLDER" ?></h2>
 		</header>
 
-		<section>
-			<style>
-				@scope {
-					:scope {
-						display: grid;
-						grid-template-areas: 
-							"remedios sintomas"
-							"consultas sintomas";
-						gap: 1em;
-					}
-				}
-			</style>
+		<section id="dashboard_content">
+			<article class="dark" id="prescricoes">
+				<h3>Prescrições</h3>
 
-			<article class="dark">
-				<style> @scope {
-					:scope {
-						grid-area: remedios;
-					}
-				} </style>
-				<p>Prescrições</p>
-
-				<div>
-					<p>Das</p>
-					<p>Das</p>
-					<p>Das</p>
+				<div class="lista">
+					<p class="mensagem">Não há prescrições</p>
 				</div>
 			</article>
 
-			<article class="dark">
-				<style> @scope {
-					:scope {
-						grid-area: consultas;
-					}
-				} </style>
-				<p>Consultas próximas</p>
+			<article class="dark" id="consultas">
+				<h3>Consultas próximas</h3>
 
-				<div>
-					<p>Das</p>
-					<p>Das</p>
-					<p>Das</p>
+				<div class="lista">
+					<p class="mensagem">Não há consultas marcadas</p>
 				</div>
 			</article>
 
-			<article class="dark">
-				<style> @scope {
-					:scope {
-						grid-area: sintomas;
-						display: flex;
-						flex-direction: column;
-						justify-content: space-between;
-					}
+			<article class="dark" id="sintomas">
+				<h3>Sintomas recentes</h3>
 
-					div {
-						display: flex;
-						flex-direction: column;
-						width: 100%;
-						gap: 0.5em;
-					}
-
-					div > article {
-						border-radius: 8px;
-					}
-				} </style>
-				<p>Sintomas recentes</p>
-
-				<div>
+				<div class="lista">
 					<article class="light">Das</article>
 					<article class="light">Das</article>
 					<article class="light">Das</article>
