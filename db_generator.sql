1dc8a73e05bd88227d5c25e87d2bd243b1654b7f
leafcabral <rafaelcabral4100@gmail.com>
2026-06-25 19:45:28 -0300

    feat: improve sql for database generator
    Make user have 0 to N contact forms.
    
    Change user password to be CHAR(64), as it stores hexadecimal string for the hash that is always 64 characters.
    
    Add ON DELETE for foreign keys constraints when needed.
    
    Change string types to enum wht possible.
    
    Make generator drop the table to always update it when running the generator.

--- db_generator.sql
+++ db_generator.sql
@@ -1,141 +1,164 @@
 CREATE DATABASE IF NOT EXISTS consultapronta_poc;
 USE consultapronta_poc;
 
-CREATE TABLE IF NOT EXISTS contato (
-    id_contato INTEGER AUTO_INCREMENT PRIMARY KEY,
-    tipo VARCHAR(20),
-    valor VARCHAR(100)
+DROP TABLE IF EXISTS usuario;
+CREATE TABLE usuario (
+	id_usuario INTEGER AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(100),
+	cpf VARCHAR(11),
+	email VARCHAR(100),
+	senha_hash CHAR(64), -- sha-265 hexadecimal
+	tipo_usuario ENUM('paciente', 'profissional'),
+	data_cadastro DATE
 );
 
-CREATE TABLE IF NOT EXISTS usuario (
-    id_usuario INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_contato INTEGER,
-    nome VARCHAR(100),
-    cpf VARCHAR(11),
-    email VARCHAR(100),
-    senha_hash VARCHAR(255),
-    tipo_usuario CHAR(2),
-    data_cadastro DATE,
+DROP TABLE IF EXISTS contato;
+CREATE TABLE contato (
+	id_contato INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_usuario INTEGER,
+	tipo VARCHAR(20),
+	valor VARCHAR(100),
 
-    CONSTRAINT FK_USUARIO_id_contato
-        FOREIGN KEY (id_contato)
-        REFERENCES contato(id_contato)
+	CONSTRAINT FK_CONTATO_id_usuario
+		FOREIGN KEY (id_usuario)
+		REFERENCES usuario(id_usuario)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS paciente (
-    id_paciente INTEGER PRIMARY KEY,
-    data_nascimento DATE,
-    altura SMALLINT,
-    peso NUMERIC(5,2),
-    alergias TEXT,
-    historico_familiar TEXT,
-    doencas TEXT,
-    tipo_sanguineo VARCHAR(3),
+DROP TABLE IF EXISTS paciente;
+CREATE TABLE paciente (
+	id_paciente INTEGER PRIMARY KEY,
+	data_nascimento DATE,
+	altura SMALLINT,
+	peso NUMERIC(5,2),
+	alergias TEXT,
+	historico_familiar TEXT,
+	doencas TEXT,
+	tipo_sanguineo VARCHAR(3),
 
-    CONSTRAINT FK_PACIENTE_id_usuario
-        FOREIGN KEY (id_paciente)
-        REFERENCES usuario(id_usuario)
+	CONSTRAINT FK_PACIENTE_id_usuario
+		FOREIGN KEY (id_paciente)
+		REFERENCES usuario(id_usuario)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS profissional (
-    id_profissional INTEGER PRIMARY KEY,
-    crm VARCHAR(10),
-    especialidades TEXT,
+DROP TABLE IF EXISTS profissional;
+CREATE TABLE profissional (
+	id_profissional INTEGER PRIMARY KEY,
+	crm VARCHAR(10),
+	especialidades TEXT,
 
-    CONSTRAINT FK_PROFISSIONAL_id_usuario
-        FOREIGN KEY (id_profissional)
-        REFERENCES usuario(id_usuario)
+	CONSTRAINT FK_PROFISSIONAL_id_usuario
+		FOREIGN KEY (id_profissional)
+		REFERENCES usuario(id_usuario)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS sintoma (
-    id_sintoma INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_paciente INTEGER,
-    descricao VARCHAR(300),
-    intensidade SMALLINT,
-    data_inicio TIMESTAMP,
-    local VARCHAR(50),
-    status VARCHAR(15),
+DROP TABLE IF EXISTS sintoma;
+CREATE TABLE sintoma (
+	id_sintoma INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_paciente INTEGER,
+	descricao VARCHAR(300),
+	intensidade SMALLINT,
+	data_inicio TIMESTAMP,
+	local VARCHAR(50),
+	status ENUM('ativo', 'inativo') DEFAULT 'ativo',
 
-    CONSTRAINT FK_SINTOMA_id_paciente
-        FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente)
+	CONSTRAINT FK_SINTOMA_id_paciente
+		FOREIGN KEY (id_paciente)
+		REFERENCES paciente(id_paciente)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS autorizacao (
-    id_autorizacao INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_paciente INTEGER,
-    id_profissional INTEGER,
-    data_concessao DATE,
-    data_revogacao DATE,
-    status VARCHAR(15),
+DROP TABLE IF EXISTS autorizacao;
+CREATE TABLE autorizacao (
+	id_autorizacao INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_paciente INTEGER,
+	id_profissional INTEGER,
+	data_concessao DATE,
+	data_revogacao DATE,
+	status ENUM('ativa', 'revogada') DEFAULT 'ativa',
 
-    CONSTRAINT FK_AUTORIZACAO_id_profissional
-        FOREIGN KEY (id_profissional)
-        REFERENCES profissional(id_profissional),
-    CONSTRAINT FK_AUTORIZACAO_id_paciente
-        FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente)
+	CONSTRAINT FK_AUTORIZACAO_id_profissional
+		FOREIGN KEY (id_profissional)
+		REFERENCES profissional(id_profissional)
+		ON DELETE CASCADE,
+	CONSTRAINT FK_AUTORIZACAO_id_paciente
+		FOREIGN KEY (id_paciente)
+		REFERENCES paciente(id_paciente)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS prescricao (
-    id_prescricao INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_paciente INTEGER,
-    id_profissional INTEGER,
-    medicamentos TEXT,
-    orientacoes_uso VARCHAR(150),
-    data_emissao DATE,
-    dados DATE,
+DROP TABLE IF EXISTS prescricao;
+CREATE TABLE prescricao (
+	id_prescricao INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_paciente INTEGER,
+	id_profissional INTEGER,
+	medicamentos TEXT,
+	orientacoes_uso VARCHAR(150),
+	data_emissao DATE,
+	dados TEXT,
 
-    CONSTRAINT FK_PRESCRICAO_id_profissional
-        FOREIGN KEY (id_profissional)
-        REFERENCES profissional(id_profissional),
-    CONSTRAINT FK_PRESCRICAO_id_paciente
-        FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente)
+	CONSTRAINT FK_PRESCRICAO_id_profissional
+		FOREIGN KEY (id_profissional)
+		REFERENCES profissional(id_profissional)
+		ON DELETE CASCADE,
+	CONSTRAINT FK_PRESCRICAO_id_paciente
+		FOREIGN KEY (id_paciente)
+		REFERENCES paciente(id_paciente)
+		ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS exame (
-    id_exame INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_paciente INTEGER,
-    id_profissional INTEGER,
-    titulo VARCHAR(100),
-    tipo VARCHAR(100),
-    descricao_resultado VARCHAR(100),
-    data_resultado DATE,
+DROP TABLE IF EXISTS exame;
+CREATE TABLE exame (
+	id_exame INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_paciente INTEGER,
+	id_profissional INTEGER,
+	titulo VARCHAR(100),
+	tipo VARCHAR(100),
+	descricao_resultado VARCHAR(100),
+	data_resultado DATE,
 
-    CONSTRAINT FK_EXAME_id_paciente
-        FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente),
-    CONSTRAINT FK_EXAME_id_profissional
-        FOREIGN KEY (id_profissional)
-        REFERENCES profissional(id_profissional)
+	CONSTRAINT FK_EXAME_id_paciente
+		FOREIGN KEY (id_paciente)
+		REFERENCES paciente(id_paciente)
+		ON DELETE CASCADE,
+	CONSTRAINT FK_EXAME_id_profissional
+		FOREIGN KEY (id_profissional)
+		REFERENCES profissional(id_profissional)
+		ON DELETE SET NULL
 );
 
-CREATE TABLE IF NOT EXISTS relatorio (
-    id_relatorio INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_paciente INTEGER,
-    data_geracao DATE,
-    titulo VARCHAR(100),
-    periodo_analisado VARCHAR(30),
-    dados_analiticos VARCHAR(200),
+DROP TABLE IF EXISTS relatorio;
+CREATE TABLE relatorio (
+	id_relatorio INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_paciente INTEGER,
+	data_geracao DATE,
+	titulo VARCHAR(100),
+	periodo_analisado VARCHAR(30),
+	dados_analiticos VARCHAR(200),
 
-    CONSTRAINT FK_RELATORIO_id_paciente
-        FOREIGN KEY (id_paciente)
-        REFERENCES paciente(id_paciente)
+	CONSTRAINT FK_RELATORIO_id_paciente
+		FOREIGN KEY (id_paciente)
+		REFERENCES paciente(id_paciente)
+		ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS anotacao_clinica (
-    id_anotacao INTEGER AUTO_INCREMENT PRIMARY KEY,
-    id_profissional INTEGER,
-    id_relatorio INTEGER,
-    data_hora TIMESTAMP,
-    texto_evolucao VARCHAR(300),
-    hipotese_diagnostica VARCHAR(100),
+DROP TABLE IF EXISTS anotacao_clinica;
+CREATE TABLE anotacao_clinica (
+	id_anotacao INTEGER AUTO_INCREMENT PRIMARY KEY,
+	id_profissional INTEGER,
+	id_relatorio INTEGER,
+	data_hora TIMESTAMP,
+	texto_evolucao VARCHAR(300),
+	hipotese_diagnostica VARCHAR(100),
 
-    CONSTRAINT FK_ANOTACAO_CLINICA_id_profissional
-        FOREIGN KEY (id_profissional)
-        REFERENCES profissional(id_profissional),
-    CONSTRAINT FK_ANOTACAO_CLINICA_id_relatorio
-        FOREIGN KEY (id_relatorio)
-        REFERENCES relatorio(id_relatorio)
+	CONSTRAINT FK_ANOTACAO_CLINICA_id_profissional
+		FOREIGN KEY (id_profissional)
+		REFERENCES profissional(id_profissional)
+		ON DELETE SET NULL,
+	CONSTRAINT FK_ANOTACAO_CLINICA_id_relatorio
+		FOREIGN KEY (id_relatorio)
+		REFERENCES relatorio(id_relatorio)
+		ON DELETE CASCADE
 );
