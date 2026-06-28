f1f9a13534d1af482236fc86b1efa7f292fc21c2
Fernando <fernandofkz@hotmail.com>
2026-06-28 11:52:51 -0300

    alterações

---
+++ public/includes/asideProfissional.php
@@ -0,0 +1,45 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'] . "/config/global.php"; ?>
+
+<style>
+	@scope (aside) {
+		:scope {
+			background-color: var(--color-primary);
+			padding: 2em 2em;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+		}
+
+		#aside_up, #aside_bottom {
+			display: flex;
+			flex-direction: column;
+			height: fit-content;
+			gap: 0.5em;
+		}
+
+		a {
+			color: white;
+			text-decoration: none;
+			padding: 10px;
+			border-radius: 10px;
+		}
+		a:hover {
+			background-color: #ffffff2e;
+		}
+	}
+</style>
+
+
+<aside>
+	<div id="aside_up">
+		<a href="/pages/paciente">Página Inicial</a>
+		<a href="\pages\profissional\exame.php">Exames</a>
+		<a href="/pages/paciente/sintoma.php">Pacientes</a>
+		<a href="/pages/paciente/relatorio.php">Farmácia</a>
+	</div>
+
+	<div id="aside_bottom">
+		<a href="/pages/paciente/perfil.php">Perfil</a>
+		<a href="/pages/sair.php">Sair</a>
+	</div>
+</aside>
