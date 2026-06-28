afbe77aad2a033b2e976b4652e29bda2ec751621
calielian <calielian@proton.me>
2026-06-28 19:08:05 -0300

    feat: added button to add new contacts on patients

--- public/pages/paciente/atualizar_perfil.php
+++ public/pages/paciente/atualizar_perfil.php
@@ -1,17 +1,24 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . "/config/global.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/config/global.php";
 verify_user_logged_in();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$id_usuario = $_SESSION["id_usuario"];
-	$secao = $_POST['secao'];
-	unset($_POST['secao']); 
+	$secao = $_POST["secao"];
+	$operacao = $_POST["operacao"];
+	unset($_POST["secao"]);
+	unset($_POST["operacao"]);
 
-	if ($secao === 'dados_saude') {
-		update_patient_data($id_usuario, $_POST);
-	} 
-	elseif ($secao === 'dados_contato') {
-		update_patient_contacts($id_usuario, $_POST);
+	switch ($operacao) {
+		case "editar":
+			if ($secao == "dados_saude") { update_patient_data($id_usuario, $_POST); } 
+			elseif ($secao == "dados_contato") { update_patient_contacts($id_usuario, $_POST); }
+			break;
+		case "adicionar":
+			if ($secao == "dados_saude") {}
+			elseif ($secao == "dados_contato") { add_new_contact($id_usuario, $_POST["tipo"], $_POST["valor"]);	}
+			break;
+		default: // cooked
 	}
 }
 
