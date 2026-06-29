e85ba0e3ffdeeb047903e7bce606415b5e3bf10a
Fernando <fernandofkz@hotmail.com>
2026-06-29 10:16:40 -0300

    alteração final

--- public/includes/aside.php
+++ public/includes/aside.php
@@ -34,7 +34,6 @@
 	<div id="aside_up">
 		<a href="/pages/paciente">Página Inicial</a>
 		<a href="/pages/paciente/sintoma.php">Registrar Sintoma</a>
-		<a href="/pages/paciente/relatorio.php">Gerar Relatório</a>
 		<a href="/pages/paciente/visualizarrelatorio.php">Relatórios</a>
 		<a href="/pages/paciente/profissionais.php">Seus Profissionais</a>
 	</div>
